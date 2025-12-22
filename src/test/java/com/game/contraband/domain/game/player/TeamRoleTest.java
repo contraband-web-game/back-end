7d9dcd6b5c5ed68bbccd9690964645aa6568cce5
apptie <appti@naver.com>
2025-12-23 01:56:09 +0900

    test: 사용하지 않는 import문 제거

--- src/test/java/com/game/contraband/domain/game/player/TeamRoleTest.java
+++ src/test/java/com/game/contraband/domain/game/player/TeamRoleTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
