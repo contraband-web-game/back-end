995d45c18ce70137dfe056ab5ad403db42c03ac4
apptie <appti@naver.com>
2025-12-23 01:10:27 +0900

    chore: 프로젝트 초기화

---
+++ src/main/java/com/game/contraband/ContrabandApplication.java
@@ -0,0 +1,13 @@
+package com.game.contraband;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContrabandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContrabandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/game/contraband/ContrabandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.contraband;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContrabandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
