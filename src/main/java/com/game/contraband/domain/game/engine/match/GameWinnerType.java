6fd6cba9a473b2cfe82f3b87528b779914ab51ae
apptie <appti@naver.com>
2025-12-24 01:50:21 +0900

    refactor: 게임 매칭 관련 도메인 패키지 변경

--- src/main/java/com/game/contraband/domain/game/engine/match/GameWinnerType.java
+++ src/main/java/com/game/contraband/domain/game/engine/match/GameWinnerType.java
@@ -1,4 +1,4 @@
-package com.game.contraband.domain.game.engine;
+package com.game.contraband.domain.game.engine.match;
 
 public enum GameWinnerType {
     SMUGGLER_TEAM,
--- src/test/java/com/game/contraband/domain/game/engine/match/ContrabandGameTest.java
+++ src/test/java/com/game/contraband/domain/game/engine/match/ContrabandGameTest.java
@@ -1,4 +1,4 @@
-package com.game.contraband.domain.game.engine;
+package com.game.contraband.domain.game.engine.match;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
