cb1b12c397104d1ef392727017d900f0e7a83e96
apptie <appti@naver.com>
2025-12-24 16:29:11 +0900

    feat: 클라이언트에서 서버로 전달하는 웹소켓 메시지 타입 정의 enum 추가

---
+++ src/main/java/com/game/contraband/infrastructure/message/WebSocketInboundMessageType.java
@@ -0,0 +1,32 @@
+package com.game.contraband.infrastructure.message;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import java.util.Arrays;
+import java.util.Optional;
+
+public enum WebSocketInboundMessageType {
+
+    HEARTBEAT_PING("PING"),
+    SESSION_HEALTH_PONG("WS_PONG");
+
+    private final String type;
+
+    WebSocketInboundMessageType(String type) {
+        this.type = type;
+    }
+
+    @JsonValue
+    public String type() {
+        return type;
+    }
+
+    public boolean isSameType(String rawType) {
+        return type.equalsIgnoreCase(rawType);
+    }
+
+    public static Optional<WebSocketInboundMessageType> from(String rawType) {
+        return Arrays.stream(values())
+                     .filter(value -> value.isSameType(rawType))
+                     .findFirst();
+    }
+}
