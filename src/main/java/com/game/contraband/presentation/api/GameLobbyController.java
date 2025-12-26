bf0bb0ef37a5ba3dd1eea60faf59597bbc92946a
apptie <appti@naver.com>
2025-12-26 20:36:15 +0900

    feat: 로비 관련 컨트롤러 추가

---
+++ src/main/java/com/game/contraband/presentation/api/GameLobbyController.java
@@ -0,0 +1,47 @@
+package com.game.contraband.presentation.api;
+
+import com.game.contraband.application.game.GameLobbyService;
+import java.util.concurrent.CompletionStage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/lobbies")
+@RequiredArgsConstructor
+public class GameLobbyController {
+
+    private final GameLobbyService gameLobbyService;
+
+    @PostMapping
+    public CompletionStage<ResponseEntity<Void>> createLobby(@RequestBody CreateLobbyRequest request) {
+        return gameLobbyService.createLobby(
+                                       request.userId(),
+                                       request.hostName(),
+                                       request.maxPlayerCount(),
+                                       request.lobbyName()
+                               )
+                               .handle(
+                                       (ignore, ex) -> ex == null
+                                               ? ResponseEntity.accepted().build()
+                                               : ResponseEntity.status(500).build()
+                               );
+    }
+
+    @PostMapping("/join")
+    public CompletionStage<ResponseEntity<Void>> joinLobby(@RequestBody JoinLobbyRequest request) {
+        return gameLobbyService.joinLobby(request.userId(), request.playerName(), request.roomId(), request.entityId())
+                               .handle(
+                                       (ignore, ex) -> ex == null
+                                               ? ResponseEntity.accepted().build()
+                                               : ResponseEntity.status(500).build()
+                               );
+    }
+
+    public record CreateLobbyRequest(Long userId, String hostName, int maxPlayerCount, String lobbyName) { }
+
+    public record JoinLobbyRequest(Long userId, String playerName, Long roomId, String entityId) { }
+}
